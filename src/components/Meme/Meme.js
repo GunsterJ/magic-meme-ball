019a1ef724bcac7bfe8bc69c8c142d1fbbf5179d
Jeroen Gunster <48288969+erole-attack@users.noreply.github.com>
2021-07-05 22:14:40 +0200

    Added Meme Component to project

---
+++ src/components/Meme/Meme.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import eightballGIF from '../../gifs/eightball.gif'
+
+const Meme = () => {
+   return (
+      <>
+         <img src={eightballGIF} alt="Otter dancing with a fish" />
+      </>
+   )
+}
+
+export default Meme
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Layout from '../components/Layout/Layout.js'
+import Meme from '../components/Meme/Meme.js'
 import React from 'react'
 import SEO from '../components/SEO/SEO.js'
 import UserInput from '../components/UserInput/UserInput.js'
@@ -6,6 +7,7 @@ import UserInput from '../components/UserInput/UserInput.js'
 const IndexPage = () => (
   <Layout>
     <SEO title='Magic Meme Ball'/>
+    <Meme/>
     <UserInput
       label='Ask the magic meme ball a polar question'
       adornment='Oh, magic meme ball'
